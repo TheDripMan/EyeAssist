8dd4e2125e9f1faaa60c8b301b83ff11105e1518
TheDripMan <133345637+TheDripMan@users.noreply.github.com>
2025-08-17 13:59:38 +0700

    workspace

---
+++ src/main/java/dev/thedripman/EyeAssist.java
@@ -0,0 +1,20 @@
+package dev.thedripman;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Block;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class EyeAssist implements ModInitializer {
+	public static final String MOD_ID = "eyeassist";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
