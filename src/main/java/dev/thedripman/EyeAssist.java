2bde6296e1788ec9bf22f2554ef78feb09087151
TheDripMan <133345637+TheDripMan@users.noreply.github.com>
2025-08-22 16:03:32 +0700

    Revert "extra options extra options extra options extra options extra options extra options extra options extra options extra options extra options extra options extra options extra options"
    This reverts commit 01454fb6909e2ac97c36a3d6c46db02fa58e7b93.

--- src/main/java/dev/thedripman/EyeAssist.java
+++ src/main/java/dev/thedripman/EyeAssist.java
@@ -1,10 +1,13 @@
 package dev.thedripman;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EyeOfEnderEntity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,11 +16,11 @@ import org.lwjgl.glfw.GLFW;
 import java.util.List;
 
 public class EyeAssist implements ClientModInitializer {
-	public static final String MOD_ID = "extra-options";
+	public static final String MOD_ID = "eyeassist";
 
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
-	public static final KeyBinding assistKey = new KeyBinding("extra-options.key.bind", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_V, "extra-options.key.category");
+	public static final KeyBinding assistKey = new KeyBinding("eyeassist.key.bind", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_V, "eyeassist.key.category");
 
 	@Override
 	public void onInitializeClient() {
